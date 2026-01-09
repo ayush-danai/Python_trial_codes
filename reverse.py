d8316c447ef9d9314a79256eb62379f3e17178c5
ayushdanai <ayushdanai@gmail.com>
2026-01-09 14:10:39 +0530

    reversing string and list code

---
+++ reverse.py
@@ -0,0 +1,7 @@
+my_string = "Hello, World!"
+reversed_string = my_string[::-1]
+print(reversed_string) # Output: !dlroW ,olleH
+
+my_list = [1, 2, 3, 4, 5]
+reversed_list = my_list[::-1]
+print(reversed_list) # Output: [5, 4, 3, 2, 1]
